a0a8507b42cbe1391be3e8e07a3de6c3b6142381
Matúš Tomlein <matus.tomlein@gmail.com>
2015-02-26 22:40:06 +0100

    Replace , with - in file names

--- lib/trello-fs/string_to_file_name.rb
+++ lib/trello-fs/string_to_file_name.rb
@@ -3,6 +3,7 @@ module TrelloFs
     def self.convert(str)
       str.gsub(/[^\w\s_-]+/, '').
         gsub(/(^|\b\s)\s+($|\s?\b)/, '\\1\\2').
+        gsub(/,/, '-').
         gsub(/\s+/, '_')
     end
   end
